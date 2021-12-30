90a4500de25eea2dfc9877216901c2597d11a4ae
JMI <ikolajm@gmail.com>
2021-12-30 18:05:02 -0500

    making adjustments to push application to heroku

--- BlogAnywhere/BlogAnywhere/settings.py
+++ BlogAnywhere/BlogAnywhere/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = env('DJANGO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['django-bloganywhere.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
